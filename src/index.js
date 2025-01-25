59daf5e14dbc734ac615f700093a35d94d4b5117
Bekzat Abdibakas <babdibakas@yandex.kz>
2025-01-26 01:00:10 +0500

    feat: installed nodemon, added endpoint register

--- src/index.js
+++ src/index.js
@@ -2,16 +2,36 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+const port = 8000
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello World!'
-    })
+app.post('/api/register', (req, res) => {
+    const {name, email, username, password} = req.body
+
+    if (!name || !email || !username || !password) {
+        return res.status(400).json({
+            message: 'All fields are required'
+        })
+    }
+
+    try {
+        res.status(200).json({
+            message: 'Successfully registered',
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: 'Internal server error',
+        });
+    }
 })
 
-app.listen(8000, ()=>{
-    console.log('Server running on port 8000')
-})
+const start = () => {
+    app.listen(port, () => {
+        console.log(`Server running on port: ${port}`)
+    })
+}
+
+start()
